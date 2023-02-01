dcaf9febe84f91f7607c5ac51763998b2305c576
Jorge Meza Bello <jorberiver@gmail.com>
2023-01-31 23:56:31 -0600

    cambios hechos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Fragment } from "react";
 import Header from "./components/layout/Header";
 import Navegacion from "./components/layout/Navegacion";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Clientes from "./components/clientes/Clientes";
 
 function App() {
   return (
-    <Fragment>
-      <Header/>
-      <div className="grid contenedor contenido-principal">
-        <Navegacion/>
-        <main className="caja-contenido col-9"></main>
-      </div>
-    </Fragment>
-
+    <Router>
+      <Fragment>
+        <Header/>
+        <div className="grid contenedor contenido-principal">
+          <Navegacion/>
+          <main className="caja-contenido col-9">
+            <Routes>
+              <Route path="/" element={<Clientes/>}/>
+            </Routes>
+          </main>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 

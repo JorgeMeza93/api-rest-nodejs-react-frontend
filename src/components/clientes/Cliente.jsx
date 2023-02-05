24c2308f500e5249668d9638c76e7ce7a2eb13aa
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-05 02:52:23 -0600

    Eliminar clientes listo

--- src/components/clientes/Cliente.jsx
+++ src/components/clientes/Cliente.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import clienteAxios from '../../config/axios';
 
 const Cliente = ({cliente}) => {
   const { _id, nombre, apellido, empresa, email, telefono }  = cliente;
+  const eliminarCliente = id => {
+    Swal.fire({
+        title: "Estás seguro?",
+        text: "Si eliminas el cliente se perderán los datos relacionados a el",
+        type: "Warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar"
+    }).then( result => {
+        if( result.value ){
+            // Lamar a Axios
+            clienteAxios.delete(`/cliente/${id}`)
+                .then( res => {
+                    Swal.fire(
+                        "Eliminado",
+                        res.data.mensaje,
+                        "success"
+                    )
+                })
+        }
+    });
+  }
   return (
     <li className='cliente'>
         <div className='info-cliente'>
@@ -15,7 +41,7 @@ const Cliente = ({cliente}) => {
             <Link to={`/clientes/editar/${_id}`} className='btn btn-azul'>
                 <i className='fas fa-pen-alt'></i>Editar Cliente
             </Link>
-            <button type='button' className='btn btn-rojo btn-eliminar'>
+            <button type='button' className='btn btn-rojo btn-eliminar' onClick={() => eliminarCliente(_id)}>
                 <i className='fas fa-times'></i>Eliminar Cliente
             </button>
         </div>

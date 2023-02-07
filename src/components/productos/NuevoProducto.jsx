e4e8d29ebb5495b9df7bc8b9fd0640ab1e9ed1a3
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-07 13:14:48 -0600

    Cambios en Nuevo producto

--- src/components/productos/NuevoProducto.jsx
+++ src/components/productos/NuevoProducto.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const NuevoProducto = () => {
+  const [producto, guardarProducto] = useState({
+    nombre: "",
+    precio: ""
+  })
+  const [archivo, guardarArchivo] = useState("");
   return (
-    <div>NuevoProducto</div>
+    <Fragment>
+      <h2>Nuevo Producto</h2>
+      <form>
+        <legend>Lena todos los cambios</legend>
+        <div className='campo'>
+          <label>Nombre: </label>
+          <input type="text" placeholder='Nombre del producto' name='nombre' />
+        </div>
+        <div className='campo'>
+          <label>Precio: </label>
+          <input type="number" placeholder='Precio del producto' name='precio' min="0.00" step="0.10" />
+        </div>
+        <div className='campo'>
+          <label>Imagen: </label>
+          <input type="file" name='imagen' />
+        </div>
+        <div className='enviar'>
+          <input type="submoit" className='btn-azul btn' value="Agregar Producto" />
+        </div>
+      </form>
+    </Fragment>
   )
 }
 

bf94fd0d28698b1ace2655c92eec54d556728678
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-11 01:41:19 -0600

    Componente de buscar producto

---
+++ src/components/pedidos/FormBuscarProducto.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function FormBuscarProducto(props){
+  return (
+    <form onSubmit={props.buscarProducto} >
+        <legend>Busca un producto y agrega una cantidad</legend>
+        <div className='campo'>
+            <label>Productos: </label>
+            <input type="text" placeholder='Nombre Productos' name='productos' onChange={props.leerDatosBusqueda} />
+        </div>
+        <input type="submit" className='btn btn-azul btn-block' value="Buscar Producto"/>
+    </form>
+  )
+}
+
+export default FormBuscarProducto
--- src/components/pedidos/NuevoPedido.jsx
+++ src/components/pedidos/NuevoPedido.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import clienteAxios from "../../config/axios";
+import FormBuscarProducto from "./FormBuscarProducto";
 
 function NuevoPedido(){
     const { id } = useParams();
@@ -11,7 +12,13 @@ function NuevoPedido(){
     }
     useEffect( () => {
         consultarAPI();
-    }, [])
+    }, []);
+    const buscarProducto = () => {
+
+    }
+    const leerDatosBusqueda = () => {
+
+    }
     return(
         <Fragment>
             <h2>Nuevo Producto</h2>
@@ -21,38 +28,32 @@ function NuevoPedido(){
                 <p>Email: {cliente.email}</p>
                 <p>Teléfono: {cliente.telefono}</p>
             </div>
-            <form>
-                <legend>Busca un Producto y agrega una cantidad</legend>
-                <div className="campo">
-                    <label>Productos: </label>
-                    <input type="text" placeholder="Nombre del Producto" name="productos" />
-                </div>
-                <ul className="resumen">
-                    <li>
-                        <div className="texto-producto">
-                            <p className="nombre">Macbook pro</p>
-                            <p className="precio">230</p>
+            <FormBuscarProducto buscarProducto={buscarProducto} leetDatosBusqueda={leerDatosBusqueda} />
+            <ul className="resumen">
+                <li>
+                    <div className="texto-producto">
+                        <p className="nombre">Macbook pro</p>
+                        <p className="precio">230</p>
+                    </div>
+                    <div className="acciones">
+                        <div className="contenedor-cantidad">
+                            <i className="fas fa-minus"></i>
+                            <input type="text" name="cantidad" />
+                            <i className="fas fa-plus"></i>
                         </div>
-                        <div className="acciones">
-                            <div className="contenedor-cantidad">
-                                <i className="fas fa-minus"></i>
-                                <input type="text" name="cantidad" />
-                                <i className="fas fa-plus"></i>
-                            </div>
-                            <button type="button" className="btn btn-rojo">
-                                <i className="fas fa-minus-circle"></i>Eliminar Producto
-                            </button>
-                        </div>
-                    </li>
-                </ul>
-                <div className="campo">
-                    <label>Total:</label>
-                    <input type="number" name="precio" placeholder="Precio" readOnly="readonly" />
-                </div>
-                <div className="enviar">
-                    <input type="submit" className="btn btn-azul" value="Agregar Pedido" />
-                </div>
-            </form>
+                        <button type="button" className="btn btn-rojo">
+                            <i className="fas fa-minus-circle"></i>Eliminar Producto
+                        </button>
+                    </div>
+                </li>
+            </ul>
+            <div className="campo">
+                <label>Total:</label>
+                <input type="number" name="precio" placeholder="Precio" readOnly="readonly" />
+            </div>
+            <div className="enviar">
+                <input type="submit" className="btn btn-azul" value="Agregar Pedido" />
+            </div>
         </Fragment>
     )
 }

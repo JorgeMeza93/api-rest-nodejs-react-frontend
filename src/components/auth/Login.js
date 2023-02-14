c837d5f4ebd496451eb967ad788099c5a8e4614a
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-14 01:43:45 -0600

    Almacenamiento de JWT en localstorage

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,15 +1,41 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react';
+import Swal from 'sweetalert2';
+import clienteAxios from '../../config/axios';
 
 const Login = () => {
-    const leerDatos = () => {
-        
+    const [credenciales, guardarCredenciales] = useState({
+        email: "",
+        password: ""
+    })
+    const leerDatos = (e) => {
+        guardarCredenciales({
+            ...credenciales,
+            [e.target.name]: e.target.value
+        })
+    }
+    const iniciarSesion = async (e) => {
+        e.preventDefault();
+        try {
+            const respuesta = await clienteAxios.post("/login", credenciales);
+            //Extraer token y almacenarlo en localstorage
+            const { token } = respuesta.data;
+            localStorage.setItem("token", token);
+            Swal.fire("Login Exitoso", "Has Iniciado Sesión", "success");
+        } catch (error) {
+            console.log(error);
+            Swal.fire({
+                icon: "error",
+                title: "Ha ocurrido un error",
+                text: error.response.data.mensaje
+            })
+        }
     }
     return (
     <Fragment>
         <div className='login'>
             <h2>Iniciar Sesión</h2>
             <div className='contenedor-formulario'>
-                <form>
+                <form onSubmit={iniciarSesion}>
                     <div className="campo">
                         <label>Email</label>
                         <input type="" name='email' placeholder='Email para iniciar sesión' required onChange={leerDatos}/>
